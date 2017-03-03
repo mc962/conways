825831b682c98d5597bac5c9024891411c07a1ea
mc962 <michaelc962@yahoo.com>
2017-03-03 16:37:02 -0500

    Style buttons and background

--- entry.js
+++ entry.js
@@ -12,14 +12,14 @@ const init = () => {
   let boardContainer = document.getElementById('canvasLife');
   let lifeBoard = new Board(boardContainer, testBoardSize);
   const mover = new Movement(lifeBoard);
-  let startButton = document.querySelector('.start-button');
+  let startButton = document.querySelector('.start-btn');
 
   startButton.addEventListener('click', () => {mover.moveCells()});
-  let pauseButton = document.querySelector('.pause-button');
+  let pauseButton = document.querySelector('.pause-btn');
   pauseButton.addEventListener('click', () => {mover.freezeCells()});
-  let clearButton = document.querySelector('.clear-button');
+  let clearButton = document.querySelector('.clear-btn');
   clearButton.addEventListener('click', () => {mover.clearCells()});
-  let menuButton  = document.querySelector('.menu-button');
+  let menuButton  = document.querySelector('.menu-btn');
   let speedSlider = document.querySelector('.speed-slider');
   speedSlider.addEventListener('change', (e) => {mover.changeSpeed(e.currentTarget.value)})
 
